5370a1249df72bf57d051c955f67f60af3c13a99
Jason Pinlac <jasonpinlacdev@gmail.com>
2020-06-09 14:09:06 -0400

    more on optionals

--- TheBasics-Optionals.playground/Contents.swift
+++ TheBasics-Optionals.playground/Contents.swift
@@ -20,12 +20,17 @@ var severResponseCode: Int? = 404
 severResponseCode = nil
 
 
+// MARK: - FORCED UNWRAPPING -
+
+
 // IF statements to check an optional with bang operator (forced unwrapping)
 // forced upwrap[ing with the bang operator is potentially dangerous and can crash your program if you try to access a variable that contains nil
 if convertedNumber != nil {
     print(convertedNumber!)
 }
 
+
+// MARK: - OPTIONAL BINDING -
 
 // optional binding if used to find out whether an optional contains a value and if so make that value avaialble as a temporary constant or vairable.
 // the syntax for optional binding is if-let constantName = someOptional { }
@@ -44,3 +49,18 @@ if let firstNumber = Int("7"), let secondNumber = Int("99"), firstNumber > secon
 } else {
     print("one of the conditions failed")
 }
+
+
+// MARK: - NIL COALESCING OPERATOR ?? -
+
+// the nil coalescing operator ?? works like the ternary operator plus it attemps to unwrap the optional and return a value.
+// either it returns the value or if the expression/variable returns nil then the provided value to returned.
+
+// This is what is happening explicitly with the ternary operator
+// var value = optionalExpression != nil ? optionalExpression! : providedValue
+
+// This is what the nil coalescing operator looks like
+// var value = optionalExpression ?? providedValue
+
+var aNumber = Int("123a") ?? 123
+print(aNumber)
